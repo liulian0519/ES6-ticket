223e5fd4942bd990362a8a3a0e7994e9777bd5a5
liulian0519 <1223342263@qq.com>
2018-01-24 16:59:48 +0800

    js preby

---
+++ tasks/util/args.js
@@ -0,0 +1,27 @@
+import yargs from 'yargs';
+
+const args = yargs
+.option('production',{
+    boolean:true,
+    default:false,
+    describe:'min all scripts'
+})
+.option('watch',{
+    boolean:true,
+    default:false,
+    describe:'watch all files'
+})
+.option('verbose',{
+    boolean:true,
+    default:false,
+    describe:'log'
+})
+.option('sourcemaps',{
+    description:'force the creation of sroucemaps'
+})
+.option('port',{
+    string:true,
+    defalut:8080,
+    describe:'server port'
+})
+.argv
