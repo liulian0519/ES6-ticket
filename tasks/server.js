9aa615702e4c4ab7db40cd2c6da59765854ceb81
liulian0519 <1223342263@qq.com>
2018-01-28 13:48:18 +0800

    server

---
+++ tasks/server.js
@@ -0,0 +1,20 @@
+//引入一些包
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import liveserver from 'gulp-live-server';
+import args from './util/args';
+
+gulp.task('server',(cb)=>{
+    if(!args.watch) return cb();
+
+    var server = liveserver.new(['--harmony','server/bin/www']);
+    server.start();
+
+    gulp.watch(['server/public/**/*.js','server/views/**/*.ejs'],function (file) {
+        server.notify.apply(server,[file]);
+    })
+//监听需要重启的文件
+    gulp.watch(['server/routes/**/*.js','server/app.js'],function () {
+        server.start.bind(server)()
+    });
+})
